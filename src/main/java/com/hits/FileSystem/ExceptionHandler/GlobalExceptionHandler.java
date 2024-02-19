e2ea0244cb06d7537848b6a08536fd2dca726027
Danil Vasilev <gbhfns90@gmail.com>
2024-02-19 17:27:14 +0700

    Добавлена обработка невалидных данных в теле запроса + изменения compose и properties

--- src/main/java/com/hits/FileSystem/ExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/hits/FileSystem/ExceptionHandler/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import com.hits.FileSystem.Models.Dto.Response.Response;
 import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -18,6 +20,15 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Response> handleMultipartException(MultipartException e){
         return new ResponseEntity<>(new Response(HttpStatus.BAD_REQUEST.value(),
                 "Запрос не является multipart запросом"), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Response> handleValidationException(MethodArgumentNotValidException ex) {
+        FieldError fieldError = ex.getBindingResult().getFieldError();
+        String errorMessage = fieldError != null ? fieldError.getDefaultMessage() : "Ошибка валидации";
+        int status = HttpStatus.BAD_REQUEST.value();
+        return new ResponseEntity<>(new Response(status, errorMessage), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(NoSuchKeyException.class)
